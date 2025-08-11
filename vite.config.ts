a9751a36bd3a2a271a01f502131748c6d9128205
Ajin Varughese <ajinvarughese91@gmail.com>
2025-08-11 22:20:59 +0530

    deploy

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/ajinvarughese.github.io/",
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
